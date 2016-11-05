777b5aa47b8be839b4afa1609ffcca097a4f1874
Matt Gallagher <matt@zqueue.com>
2016-11-06 09:56:36 +1100

    Created Xcode project.

---
+++ CwlSignal/CwlSignal.h
@@ -0,0 +1,19 @@
+//
+//  CwlSignal.h
+//  CwlSignal
+//
+//  Created by Matt Gallagher on 11/6/16.
+//  Copyright © 2016 Matt Gallagher ( http://cocoawithlove.com ). All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CwlSignal.
+FOUNDATION_EXPORT double CwlSignalVersionNumber;
+
+//! Project version string for CwlSignal.
+FOUNDATION_EXPORT const unsigned char CwlSignalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CwlSignal/PublicHeader.h>
+
+
