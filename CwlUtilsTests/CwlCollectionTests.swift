56acd0cf744fcac3cdc9ee174b309032c9ff6710
Matt Gallagher <matt@zqueue.com>
2016-11-06 12:36:18 +1100

    Squashed 'CwlUtils/' content from commit 509a4cc
    git-subtree-dir: CwlUtils
    git-subtree-split: 509a4cc8c9ef678d321686647ee0296424958dc7

---
+++ CwlUtilsTests/CwlCollectionTests.swift
@@ -0,0 +1,33 @@
+//
+//  CwlCollectionTests.swift
+//  CwlUtils
+//
+//  Created by Matt Gallagher on 2015/02/03.
+//  Copyright © 2015 Matt Gallagher ( http://cocoawithlove.com ). All rights reserved.
+//
+//  Permission to use, copy, modify, and/or distribute this software for any
+//  purpose with or without fee is hereby granted, provided that the above
+//  copyright notice and this permission notice appear in all copies.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+//  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+//  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+//  SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+//  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+//  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+//  IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+//
+
+import Foundation
+import XCTest
+import CwlUtils
+
+class CollectionTests: XCTestCase {
+	func testAtCollection() {
+		let a = 7..<9
+		let at_0 = a.at(7)
+		let at_2 = a.at(9)
+		let at_1 = a.at(-1)
+		XCTAssert(at_0! == 7 && at_1 == nil && at_2 == nil)
+	}
+}
