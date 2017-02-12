68c801301021225d91f442240664f337b5f1cb1d
Matt Gallagher <matt@zqueue.com>
2017-02-12 17:42:22 +1100

    Trying dependency fetch as its own "aggregate" target.

--- Package.swift
+++ Package.swift
@@ -3,13 +3,15 @@ import PackageDescription
 let package = Package(
 	name: "CwlSignal",
 	dependencies: [
-		.Package(url: "https://github.com/mattgallagher/CwlUtils.git", majorVersion: 1),
+		.Package(url: "/Users/matt/Projects/CwlUtils", majorVersion: 1),
 	],
 	exclude: [
 		"LICENSE.txt",
+		"ReadMe.md",
 		"CwlSignal.playground",
+		"Sources/CwlPackageFetch",
 		"Sources/CwlSignal/CwlSignal.h",
 		"Sources/CwlSignal/Info.plist",
-		"Tests/CwlSignal/Info.plist"
+		"Tests/CwlSignalTests/Info.plist"
 	]
 )
