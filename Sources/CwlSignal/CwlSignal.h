37922adce70753ac5bdae67e047b48d4f6d6ce15
Matt Gallagher <matt@zqueue.com>
2017-02-14 19:13:51 +1100

    Minor Xcode 8.3 beta tidy ups.

--- Sources/CwlSignal/CwlSignal.h
+++ Sources/CwlSignal/CwlSignal.h
@@ -28,6 +28,6 @@ FOUNDATION_EXPORT const unsigned char CwlSignalVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CwlSignal/PublicHeader.h>
 
-#if DEBUG
+#if !OMIT_STACK_FRAME
 #import "CwlFrameAddress.h"
 #endif
