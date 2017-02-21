d12d5e0fdf40a16b01a94eeed5c9663c5ee234a0
Matt Gallagher <matt@zqueue.com>
2017-02-21 12:03:36 +1100

    Removed "OMIT_STACK_FRAME" from the header in this project (it will be contained entirely in CwlUtils).

--- Sources/CwlSignal/CwlSignal.h
+++ Sources/CwlSignal/CwlSignal.h
@@ -28,6 +28,4 @@ FOUNDATION_EXPORT const unsigned char CwlSignalVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CwlSignal/PublicHeader.h>
 
-#if !OMIT_STACK_FRAME
 #import "CwlFrameAddress.h"
-#endif
