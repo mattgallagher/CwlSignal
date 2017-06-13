03da9df165a82b3d0122ae0a4219b69e5ff458e8
Matt Gallagher <matt@zqueue.com>
2017-06-13 12:57:05 +1000

    Modified package resolve process.

--- Package.swift
+++ Package.swift
@@ -1,16 +1,20 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
 	name: "CwlSignal",
+	products: [.library(name: "CwlSignal", targets: ["CwlSignal"])],
 	dependencies: [
-		.Package(url: "https://github.com/mattgallagher/CwlUtils.git", Version(1, 1, 0, prereleaseIdentifiers: ["beta", "4"])),
+		.package(url: "/Users/matt/Projects/CwlUtils", .revision("94bf0e1ca0a194601caeb89a971323cfa73de3a2")),
 	],
-	exclude: [
-		"LICENSE.txt",
-		"ReadMe.md",
-		"CwlSignal.playground",
-		"Sources/CwlSignal/CwlSignal.h",
-		"Sources/CwlSignal/Info.plist",
-		"Tests/CwlSignalTests/Info.plist"
+	targets: [
+		.target(name: "CwlSignal", exclude: [
+			"CwlSignal.h",
+			"Info.plist",
+		]),
+		.testTarget(name: "CwlSignalTests", dependencies: ["CwlSignal"], exclude: [
+			"Info.plist"
+		]),
 	]
 )
+
