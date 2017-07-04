3d2df54e53da7cffdc647e90be8848fa7d2eb0ab
Matt Gallagher <matt@zqueue.com>
2017-07-05 02:00:37 +1000

    Final beta? This time for reals?

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "CwlSignal",
 	dependencies: [
-		.Package(url: "https://github.com/mattgallagher/CwlUtils.git", Version(1, 1, 0, prereleaseIdentifiers: ["beta", "15"])),
+		.Package(url: "https://github.com/mattgallagher/CwlUtils.git", Version(1, 1, 0, prereleaseIdentifiers: ["beta", "16"])),
 	],
 	exclude: [
 		"LICENSE.txt",
