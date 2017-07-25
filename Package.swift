c83c5b5c34485790d3e23c5a7277871ec15e4ece
Matt Gallagher <matt@zqueue.com>
2017-07-25 14:52:59 +1000

    Minor improvements to building of fetched targets.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "CwlSignal",
 	dependencies: [
-		.Package(url: "https://github.com/mattgallagher/CwlUtils.git", Version(1, 1, 0, prereleaseIdentifiers: ["beta", "19"])),
+		.Package(url: "https://github.com/mattgallagher/CwlUtils.git", Version(1, 1, 0, prereleaseIdentifiers: ["beta", "20"])),
 	],
 	exclude: []
 )
