db54de3b990e86f99af82c5fc38ae0e5b1dd89c6
Matt Gallagher <matt@zqueue.com>
2017-12-21 20:43:00 +1100

    Timeline, playground and concat script improvements.

--- CwlSignal.playground/Pages/App scenario - dynamic view properties.xcplaygroundpage/Contents.swift
+++ CwlSignal.playground/Pages/App scenario - dynamic view properties.xcplaygroundpage/Contents.swift
@@ -6,23 +6,31 @@
 
 ## Dynamic view properties
 
-This page creates and maintains a view with an "Add to favorites" button that is enabled if (and only if) both of the following are true:
+This page shows how to implement user-interface controls dependent on multiple sources of changing data using CwlSignal.
 
-1. the user is logged in
-2. the file selection is non-empty
+There are two data dependencies in the example:
 
-The login status and the number of selected files are also displayed in the view as a checkbox and a text label. Neither of these are "real" (they cycle through states on a timer) but they should allow you to see how everything is connected and updated.
+1. User login status (changes on a background thread)
+2. File selection (changes on the main thread)
 
-The most important part of the code are the three "dynamic properties" at the bottom of the `loadView` function. These take data provided by incoming signals and apply the result to the views.
+In this example, the login is just a `Bool` and the file selection is just an array of `Int` – and they are automatically cycled on a timer – but they should allow you to see how everything is connected and updated.
+
+There are three user-interface controls dependent on this data:
+
+1. Checkbox (checked if user is logged in)
+2. Text label (show the count of selected files)
+3. Add to favorites button (enabled only if user is logged in *and* file selection is non-empty).
+
+If you're reading through the code, the most important part to focus on is the three "dynamic properties" at the bottom of the `loadView` function. These take data provided by incoming signals and apply the result to the views.
 
 ## Using the Assistant View
 
-To display the "view" for this playgrounds page, you should enable the Playgrounds "Timeline" Assistant View. To do this,
+To display the "view" for this playgrounds page, you should enable the Playgrounds "Live View" Assistant View. To do this,
 
 1. Make sure you can see Assistant Editor (from the menubar: "View" → "Assistant Editor" → "Show Assistant Editor").
-2. Use the toolbar at the top of the Assistant Editor to select "Timeline" (the popup you need is to the right of the "< >" arrows but to the left of the filename/filepath.
+2. Use the toolbar at the top of the Assistant Editor to select "Live View" (the popup you need is to the right of the "< >" arrows but to the left of the filename/filepath.
 
-After the page has run, you should be able to use the "timeline" slider to move forwards and backwards in time and see how the view state changed in response to the two state values.
+The page will automatically animate through a series of states.
 
 ---
  */
@@ -30,31 +38,22 @@ import Cocoa
 import CwlSignal
 import PlaygroundSupport
 
-// Create an instance of our view controller and handle a few other Swift 3/4 differences on macOS.
-#if swift(>=4)
-	let controller = ViewController(nibName: nil, bundle: nil)
-	let center = NSStackView.Gravity.center
-	let required = NSLayoutConstraint.Priority.required
-	let onState = NSControl.StateValue.on
-	let offState = NSControl.StateValue.off
-#else
-	let controller = ViewController(nibName: nil, bundle: nil)!
-	let center = NSStackViewGravity.center
-	let required = NSLayoutPriorityRequired
-	let onState = NSOnState
-	let offState = NSOffState
-#endif
-	
-PlaygroundPage.current.liveView = controller.view
+PlaygroundPage.current.liveView = ViewController(nibName: nil, bundle: nil).view
 
-// This is a dummy Login class. Every 1.5 seconds, it toggles login on the background thread
+// This is a dummy Login class. Every 3.0 seconds, it toggles login on the background thread
 class Login {
-	let signal = Signal.interval(.fromSeconds(1.5)).map { v in v % 2 == 0 }.continuous(initialValue: false)
+	let signal = Signal
+		.interval(.fromSeconds(3.0))
+		.map { v in v % 2 == 0 }
+		.continuous(initialValue: false)
 }
 
-// This is a FileSelection class. Every 0.5 seconds, it changes the number of selected files on the main thread
+// This is a FileSelection class. Every 0.75 seconds, it changes the number of selected files on the main thread
 class FileSelection {
-	let signal = Signal.interval(.fromSeconds(0.5), context: .main).map { v in Array<Int>(repeating: 0, count: v % 3) }.continuous(initialValue: Array<Int>())
+	let signal = Signal
+		.interval(.fromSeconds(0.75), context: .main)
+		.map { v in Array<Int>(repeating: 0, count: v % 3) }
+		.continuous(initialValue: Array<Int>())
 }
 
 class ViewController: NSViewController {
@@ -74,17 +73,17 @@ class ViewController: NSViewController {
 		
 		// Set static properties
 		view.orientation = .vertical
-		view.setHuggingPriority(required, for: .horizontal)
+		view.setHuggingPriority(.required, for: .horizontal)
 
 		// Construct the view tree
-		view.addView(addToFavoritesButton, in: center)
-		view.addView(loggedInStatusButton, in: center)
-		view.addView(filesSelectedLabel, in: center)
+		view.addView(addToFavoritesButton, in: .center)
+		view.addView(loggedInStatusButton, in: .center)
+		view.addView(filesSelectedLabel, in: .center)
 		view.layoutSubtreeIfNeeded()
 		
 		// Configure dynamic properties
 		endpoints += login.signal.subscribe(context: .main) { r in
-			self.loggedInStatusButton.state = (r.value ?? false) ? onState : offState
+			self.loggedInStatusButton.state = (r.value ?? false) ? .on : .off
 		}
 		endpoints += fileSelection.signal.subscribe(context: .main) { r in
 			self.filesSelectedLabel.stringValue = "Selected file count: \(r.value?.count ?? 0)"
