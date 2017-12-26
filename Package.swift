8cdfb39bd37ebb2950e1a42035ea0101af20cd5b
Matt Gallagher <matt@zqueue.com>
2017-12-26 23:01:55 +1100

    Updated to Swift 4 package format and latest CwlUtils revision.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,16 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-	name: "CwlSignal",
-	dependencies: [
-		.Package(url: "https://github.com/mattgallagher/CwlUtils.git", Version(1, 1, 0, prereleaseIdentifiers: ["beta", "28"])),
-	],
-	exclude: []
+   name: "CwlSignal",
+   products: [.library(name: "CwlSignal", type: .dynamic, targets: ["CwlSignal"])],
+	dependencies: [.package(url: "https://github.com/mattgallagher/CwlUtils.git", .revision("21fa87616a3aed2a79c64e6fd7d4837093d160bd"))],
+	targets: [
+		.target(
+			name: "CwlSignal",
+			dependencies: [
+				.product(name: "CwlUtils")
+			]
+		)
+	]
 )
