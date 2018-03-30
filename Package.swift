c3aaa3cd97fdf40f1f4440b991ff81d19fce2338
Matt Gallagher <matt@zqueue.com>
2018-03-30 16:52:47 +1100

    Picked up the latest CwlUtils to get the Deque fixed for Swift 4.1.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
    name: "CwlSignal",
    products: [.library(name: "CwlSignal", type: .dynamic, targets: ["CwlSignal"])],
-	dependencies: [.package(url: "https://github.com/mattgallagher/CwlUtils.git", .revision("f198875359f4290e67c30e3571a6b061d9dc97ee"))],
+	dependencies: [.package(url: "https://github.com/mattgallagher/CwlUtils.git", .revision("e88c7369f10447dcf5f697d29b3c54176ca42a49"))],
 	targets: [
 		.target(
 			name: "CwlSignal",
