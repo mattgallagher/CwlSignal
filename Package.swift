c4a1a2cbf48c914fcebdd7df83ddf8218f0f9886
Matt Gallagher <matt@zqueue.com>
2019-04-28 09:59:50 +1000

    Updated for 2.2 release.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
    	.library(name: "CwlSignal", type: .dynamic, targets: ["CwlSignal"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/mattgallagher/CwlUtils.git", from: "2.0.0"),
+		.package(url: "https://github.com/mattgallagher/CwlUtils.git", from: "2.2.0"),
 		.package(url: "https://github.com/mattgallagher/CwlPreconditionTesting.git", from: "1.1.0"),
 	],
 	targets: [
